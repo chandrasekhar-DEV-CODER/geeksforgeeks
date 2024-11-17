34a1c0af95dceb7576b9288098c6eba17ee34d18
chandrasekhar-DEV-CODER <chandrasekharsathi.dev@gmail.com>
2024-11-17 18:53:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap kth elements/swap-kth-elements.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void swapKth(int k, vector<int> &arr) {
+        // code here
+        swap(arr[k-1],arr[arr.size()-k]);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int k;
+        cin >> k;
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.swapKth(k, arr);
+        for (auto x : arr) {
+            cout << x << " ";
+        }
+        cout << endl << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
