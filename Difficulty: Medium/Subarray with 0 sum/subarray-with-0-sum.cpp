f9d720f7732be7e0710d6853601faee6f9ed7fdf
chandrasekhar-DEV-CODER <chandrasekharsathi.dev@gmail.com>
2024-11-19 20:23:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Complete this function
+    // Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(vector<int>& arr) {
+        // Your code here
+        unordered_set<int>h;
+        int sum=0;
+        for(int i:arr)
+        {
+            sum+=i;
+            if(sum==0 ||h.count(sum)||i==0)
+            {
+                return true;
+            }
+            h.insert(sum);
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        if (obj.subArrayExists(arr))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
